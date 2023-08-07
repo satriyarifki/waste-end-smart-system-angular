707c6dae1222dbbf06e0145e9e357af41f94a2b8
satriyarifki <rifkisatriya@gmail.com>
2023-08-07 16:42:20 +0700

    feat(Alur System) add image and flow

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { PrintTransactionInComponent } from './print/print-transaction-in/print-transaction-in.component';
 import { ScalesPassboxComponent } from './scales-passbox/scales-passbox.component';
 import { StockBarangComponent } from './stock-barang/stock-barang.component';
+import { SystemFlowComponent } from './system-flow/system-flow.component';
 import { TransactionInComponent } from './transaction-in/transaction-in.component';
 import { TransactionOutComponent } from './transaction-out/transaction-out.component';
 
@@ -22,6 +23,7 @@ const routes: Routes = [
   { path: 'transaction-out/create', component: CreateTransactionOutComponent },
   { path: 'transaction-in/print/:id', component: PrintTransactionInComponent },
   { path: 'scales-passbox', component: ScalesPassboxComponent },
+  { path: 'system-flow', component: SystemFlowComponent },
 ];
 
 @NgModule({
