75ef28ad5a69a7ec23d6dac6caa4c3516b9ef58d
satriyarifki <rifkisatriya@gmail.com>
2023-08-16 12:57:24 +0700

    fix(print) change loop data with actual one

--- src/app/print/print-transaction-in/print-transaction-in.component.ts
+++ src/app/print/print-transaction-in/print-transaction-in.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { ActualAug } from 'src/app/dashboard-sales/db_sales';
 
 const data = [
   { nama: 'Jerigen', qty: 20, satuan: 'Kg', harga: 17500 },
@@ -22,15 +23,17 @@ const data = [
 })
 export class PrintTransactionInComponent {
   dateNow = new Date();
+
+  //API
   dataRekap = data;
-  dataJual = history.state;
+  dataSales = ActualAug;
+  paramSales = history.state;
 
   constructor(private router: Router) {
     console.log(history.state);
     // console.log(location.getState());
-    console.log(new Date(this.dataJual.date));
 
-    console.log(router.getCurrentNavigation()?.extras.state);
+    console.log(this.filterSalesByDate(this.paramSales.date));
   }
   ngOnInit() {
     // console.log(history.state);
@@ -38,5 +41,28 @@ export class PrintTransactionInComponent {
   }
   toDefaultDate(date: any) {
     return new Date(date);
+  }
+
+  SalesToArrayByDate(date: any): any[] {
+    let dataFilter = this.dataSales.filter((val) => val.date == date)[0];
+    let dataArray: any[] = [];
+    dataArray.push(
+      dataFilter.balok,
+      dataFilter.besi,
+      dataFilter.botol,
+      dataFilter.drum,
+      dataFilter.karton,
+      dataFilter.palet_kayu,
+      dataFilter.palet_plastik,
+      dataFilter.plastik,
+      dataFilter.preform,
+      dataFilter.resin,
+      dataFilter.sak_besar,
+      dataFilter.sak_kecil
+    );
+    return dataArray;
+  }
+  filterSalesByDate(date: any){
+    return this.dataSales.filter((val) => val.date == date)[0];
   }
 }
