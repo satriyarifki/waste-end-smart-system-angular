e6d885ac2e82a5f164c2ea7cf092c4fbe6b4e971
satriyarifki <rifkisatriya@gmail.com>
2023-09-25 16:45:51 +0700

    fix(transaction out) repair the logic form and connect database post

--- src/app/create/create-transaction-out/create-transaction-out.component.ts
+++ src/app/create/create-transaction-out/create-transaction-out.component.ts
@@ -12,9 +12,8 @@ import { ApiService } from 'src/app/services/api.service';
   styleUrls: ['./create-transaction-out.component.css'],
 })
 export class CreateTransactionOutComponent {
-
   //FORM
-  form!:FormGroup
+  form!: FormGroup;
 
   //API
   arrayItem: any[] = [];
@@ -30,20 +29,24 @@ export class CreateTransactionOutComponent {
     spinner.show();
     this.arrayItem.push('item', '', '', '');
     console.log(this.arrayItem);
-    this.initialForm()
-    forkJoin(apiService.salesIdGet(1),apiService.vendorGet(),apiService.priceGet()).subscribe(
-      ([sales,vendor,price]) => {
+    this.initialForm();
+    forkJoin(
+      apiService.salesIdGet(1),
+      apiService.vendorGet(),
+      apiService.priceGet()
+    ).subscribe(
+      ([sales, vendor, price]) => {
         this.salesApi = sales;
-        this.vendorsApi = vendor
-        this.pricesApi = price
+        this.vendorsApi = vendor;
+        this.pricesApi = price;
         console.log(this.vendorsApi);
         console.log(this.pricesApi);
         spinner.hide();
-        console.log(this.salesToArray);
+        console.log(this.filterVendorById(2).price['resin']);
       },
       (err) => {
         console.log(err);
-        
+
         this.alertService.onCallAlert(
           'Data cannot loaded, server error !',
           AlertType.Error
@@ -51,42 +54,160 @@ export class CreateTransactionOutComponent {
           spinner.hide();
       }
     );
-    
-    
   }
 
-  get f(){
-    return this.form.value
+  get f() {
+    return this.form.value;
   }
 
-  initialForm(){
+  initialForm() {
     this.form = this.formBuilder.group({
-      date: ['',Validators.required ],
-      vendorId: [0, Validators.required ]
-    })
+      date: ['', Validators.required],
+      vendorId: [0, Validators.required],
+      alumunium: [0, Validators.min(0)],
+      balok: [0, Validators.min(0)],
+      besi: [0, Validators.min(0)],
+      botol_plastik: [0],
+      cap: [0],
+      drum: [0],
+      jerigen: [0],
+      karton: [0],
+      kertas: [0],
+      pallet_kayu: [0],
+      pallet_plastik: [0],
+      plastik: [0],
+      preform: [0],
+      resin: [0],
+      sak_besar: [0],
+      sak_kecil: [0],
+      seng: [0],
+      tembaga: [0],
+      total_price: [0, Validators.required],
+    });
   }
 
   get salesToArray(): any[] {
     // let {dataFilter = this.dataSales.filter((val) => val.date == date)[0];
     let dataArray: any[] = [];
     dataArray.push(
-      {name: 'Balok',qty: this.salesApi?.balok, price: this.salesApi?.balok, satuan: 'kg'},
-      {name: 'Besi',qty: this.salesApi?.besi, price: this.salesApi?.besi, satuan: 'kg'},
-      {name: 'Botol Plastik',qty: this.salesApi?.botol_plastik, price: this.salesApi?.botol_plastik, satuan: 'kg'},
-      {name: 'Drum',qty: this.salesApi?.drum, price: this.salesApi?.drum, satuan: 'pcs'},
-      {name: 'Jerigen',qty: this.salesApi?.jerigen, price: this.salesApi?.jerigen, satuan: 'pcs'},
-      {name: 'Karton',qty: this.salesApi?.karton, price: this.salesApi?.karton, satuan: 'kg'},
-      {name: 'Pallet Kayu',qty: this.salesApi?.pallet_kayu, price: this.salesApi?.pallet_kayu, satuan: 'pcs'},
-      {name: 'Pallet Plastik',qty: this.salesApi?.pallet_plastik, price: this.salesApi?.pallet_plastik, satuan: 'pcs'},
-      // {name: 'Plastik',qty: this.salesApi?.plastik, price: this.salesApi?.plastik, satuan: 'kg'},
-      {name: 'Preform',qty: this.salesApi?.preform, price: this.salesApi?.preform, satuan: 'kg'},
-      {name: 'Resin',qty: this.salesApi?.resin, price: this.salesApi?.resin, satuan: 'kg'},
-      {name: 'Sak Besar',qty: this.salesApi?.sak_besar, price: this.salesApi?.sak_besar, satuan: 'pcs'},
-      {name: 'Sak Kecil',qty: this.salesApi?.sak_kecil, price: this.salesApi?.sak_kecil, satuan: 'pcs'}
+      {
+        formName: 'balok',
+        name: 'Balok',
+        qty: this.salesApi?.balok,
+        price: this.salesApi?.balok,
+        satuan: 'kg',
+      },
+      {
+        formName: 'besi',
+        name: 'Besi',
+        qty: this.salesApi?.besi,
+        price: this.salesApi?.besi,
+        satuan: 'kg',
+      },
+      {
+        formName: 'botol_plastik',
+        name: 'Botol Plastik',
+        qty: this.salesApi?.botol_plastik,
+        price: this.salesApi?.botol_plastik,
+        satuan: 'kg',
+      },
+      {
+        formName: 'drum',
+        name: 'Drum',
+        qty: this.salesApi?.drum,
+        price: this.salesApi?.drum,
+        satuan: 'pcs',
+      },
+      {
+        formName: 'jerigen',
+        name: 'Jerigen',
+        qty: this.salesApi?.jerigen,
+        price: this.salesApi?.jerigen,
+        satuan: 'pcs',
+      },
+      {
+        formName: 'karton',
+        name: 'Karton',
+        qty: this.salesApi?.karton,
+        price: this.salesApi?.karton,
+        satuan: 'kg',
+      },
+      {
+        formName: 'pallet_kayu',
+        name: 'Pallet Kayu',
+        qty: this.salesApi?.pallet_kayu,
+        price: this.salesApi?.pallet_kayu,
+        satuan: 'pcs',
+      },
+      {
+        formName: 'pallet_plastik',
+        name: 'Pallet Plastik',
+        qty: this.salesApi?.pallet_plastik,
+        price: this.salesApi?.pallet_plastik,
+        satuan: 'pcs',
+      },
+      {
+        formName: 'preform',
+        name: 'Preform',
+        qty: this.salesApi?.preform,
+        price: this.salesApi?.preform,
+        satuan: 'kg',
+      },
+      {
+        formName: 'resin',
+        name: 'Resin',
+        qty: this.salesApi?.resin,
+        price: this.salesApi?.resin,
+        satuan: 'kg',
+      },
+      {
+        formName: 'sak_besar',
+        name: 'Sak Besar',
+        qty: this.salesApi?.sak_besar,
+        price: this.salesApi?.sak_besar,
+        satuan: 'pcs',
+      },
+      {
+        formName: 'sak_kecil',
+        name: 'Sak Kecil',
+        qty: this.salesApi?.sak_kecil,
+        price: this.salesApi?.sak_kecil,
+        satuan: 'pcs',
+      }
     );
     // console.log(dataArray);
-    
+
     return dataArray;
+  }
+
+  onSubmit() {
+    let total: number = 0;
+    this.salesToArray.forEach((item) => {
+      total +=
+        this.filterVendorById(this.f.vendorId)?.price[item.formName] *
+        this.f[item.formName];
+    });
+    this.f.total_price = total;
+    console.log(this.f);
+
+    if (
+      this.form.invalid
+    ) {
+      console.log(this.form);
+
+      return;
+    }
+    console.log(this.f);
+    // return
+    this.apiService.salesPost(this.f).subscribe(
+      (data) => {
+        console.log('success');
+        this.alertService.onCallAlert('Create Success!!',AlertType.Success)
+      },
+      (err) => {
+        console.log(err);
+      }
+    );
   }
 
   plusItemLoop() {
@@ -101,6 +222,9 @@ export class CreateTransactionOutComponent {
     const t = this.arrayItem.splice(index, index);
 
     console.log(this.arrayItem);
+  }
+  filterVendorById(id: any) {
+    return this.vendorsApi.filter((data) => data.id == id)[0];
   }
   popItemLoop() {
     // this.itemLoop--;
